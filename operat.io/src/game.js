3fc23f1e02d9488e5bb6f788b2dd01b674f4ad1c
Vinicius <vini.pessoa7@gmail.com>
2020-05-02 13:21:51 -0400

    fix: design fixes

--- operat.io/src/game.js
+++ operat.io/src/game.js
@@ -5,17 +5,20 @@ class Game extends React.Component {
 	render () {
 		return (
 			<div className='main-container'>
-				<div className='inner-box'>
+				<div className='box'>
 					<div className='box-container'>
 						<div className='num1-container txt'>1304</div>
-						<div className="op">+</div>
+						<div className="op">
+							<p>+</p>
+						</div>
 						<input className='txt input-container ' />
-						<div className='op'>=</div>
+						<div className='op'>
+							<p>=</p>
+						</div>
 						<div className='result-container txt'>13040</div>
-						<button className='txt btn-calculate'>Calcular</button>
 					</div>
 				</div>
-    	</div>
+    		</div>
 		);
 	}
 }
